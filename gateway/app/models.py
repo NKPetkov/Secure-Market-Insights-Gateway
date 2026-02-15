bf392665cc1dd72cfbbc6c0a4c47a6b8632d353f
Nikolay Petkov <nikolai.k.petkov@gmail.com>
2026-02-15 18:52:23 +0200

    feat: added fetcher API data models for healthcheck and insights input/output data

---
+++ gateway/app/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class CryptoInsightInput(BaseModel):
+    """Normalized cryptocurrency insight input data."""
+    symbol: str = Field(min_length=1, max_length=50, description="Symbol name")
+
+
+class CryptoInsightOutput(BaseModel):
+    """Normalized cryptocurrency insight output data."""
+
+    symbol: str                         = Field(..., description="Cryptocurrency symbol (e.g., bitcoin)")
+    name: str                           = Field(..., description="Cryptocurrency name")
+    category: str                       = Field(..., description="Cryptocurrency category, eg. coin, token")
+    description: str                    = Field(..., description="Cryptocurrency description")
+    date_launched: str                  = Field(..., description="Launch date of hte cryptocurrency")
+    logo: Optional[str]                 = Field(None, description="Cryptocurrency logo")
+    platform: Optional[str]             = Field(None, description="Name of the platform hosting the cryptocurrency")
+    circulating_suply: Optional[float]  = Field(None, description="Suply of the cryptocurrency in circulation")
+    market_cap: Optional[float]         = Field(None, description="Cryptocurrency market cap")
+
+
+class HealthResponse(BaseModel):
+    """Health check response."""
+
+    status: str
+    timestamp: datetime
